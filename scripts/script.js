63f26170ac9224dbee18432607c2c5fbd7267723
akademik2607 <akademik2607@mail.ru>
2020-12-28 10:46:34 +0300

    Выполнил задание к 19 уроку

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', function(){
     'use strict';
-    //Тайтер
+    //Таймер
     const timerHours = document.querySelector('#timer-hours'),
         timerMinutes = document.querySelector('#timer-minutes'),
         timerSeconds = document.querySelector('#timer-seconds');
@@ -20,11 +20,7 @@ window.addEventListener('DOMContentLoaded', function(){
 
         const timer = getTimeRemaining('23 december 2020 22:03');
 
-        let indexInterval; 
-        if(!indexInterval && timer.timeRemaining > 0){ 
-            indexInterval = setInterval(updateClock, 1000);     
-        }
-        else if(timer.timeRemaining < 0){
+        if(timer.timeRemaining < 0){
             console.log(indexInterval);
             clearInterval(indexInterval);
             console.log(indexInterval);
@@ -40,12 +36,76 @@ window.addEventListener('DOMContentLoaded', function(){
 
     }
 
-    //меню
+        let indexInterval = setInterval(updateClock, 1000);     
+
+   
+    updateClock();
+
+ //меню
     const toggleMenu = () => {
-        const btnMenu = document.querySelector('.btn-menu'),
-            menu = document.querySelector('menu');
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+        const actionMenu = () =>{
+            /*if(!menu.style.transform || menu.style.transform === 'translate(-100%)'){
+                menu.style.transform = 'translate(0)';
+            } else{
+                menu.style.transform = 'translate(-100%)';
+            }*/
+            menu.classList.toggle('active-menu');
+
+        };
+
+        btnMenu.addEventListener('click', actionMenu); 
+        closeBtn.addEventListener('click', actionMenu);
+
+        menuItems.forEach((item) => {
+            item.addEventListener('click', actionMenu);
+        });
 
     };
-    updateClock();
+    toggleMenu();    
+
+//popup
+    function popupAnimation() {
+        const popUpContent = document.querySelector('.popup-content');
+            popUpContent.style.top = '-100%';
+        const indexInterval = setInterval(startAnimation, 10);
+        function startAnimation (){
+            console.log('indexInervalPopup: ' + indexInterval); 
+            popUpContent.style.top = `${popUpContent.offsetTop + 5}px`; 
+            console.log(popUpContent.offsetTop);
+            console.log(indexInterval);
+            if(popUpContent.offsetTop > 50) clearInterval(indexInterval);
+        };
+    }
+
+
+    const togglePopUp = () => {
+        const popup = document.querySelector('.popup'), 
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popUpClose = document.querySelector('.popup-close');
+        console.dir(popup);
+        popupBtn.forEach((element) => {
+            element.addEventListener('click', () => {
+
+
+                popup.style.display = 'block';
+
+                if(window.screen.width >= 768)popupAnimation();
+            });
+        });
+        popUpClose.addEventListener('click', () => {
+            popup.style.display = 'none';
+        });
+    };
+    
+    
+togglePopUp();
+
+
+
+
     });
 
